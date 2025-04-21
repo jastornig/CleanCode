9d9292b65799a8c9190db7e7f0c6aae483061bcb
paulxyh <77801088+paulxyh@users.noreply.github.com>
2025-04-21 22:44:11 +0200

    PageResult

--- src/main/java/paulxyh/model/PageResult.java
+++ src/main/java/paulxyh/model/PageResult.java
@@ -1,6 +1,35 @@
 package paulxyh.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PageResult {
-//    TODO: Implement
-//      - Acts as storage for the interim results, before being written to the report
+    private final String url;
+    private final int depth;
+    private final List<PageElement> elements = new ArrayList<>();
+    private final List<PageResult> children = new ArrayList<>();
+
+    public PageResult(String url, int depth) {
+        this.url = url;
+        this.depth = depth;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+    public int getDepth() {
+        return depth;
+    }
+    public List<PageElement> getElements(){
+        return this.elements;
+    }
+    public List<PageResult> getChildren(){
+        return this.children;
+    }
+    public void addElement(PageElement element){
+        this.elements.add(element);
+    }
+    public void addChild(PageResult child){
+        this.children.add(child);
+    }
 }
