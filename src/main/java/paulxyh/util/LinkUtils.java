e5beb601c735f394473b0162983fcb5d98354e94
paulxyh <77801088+paulxyh@users.noreply.github.com>
2025-04-21 22:44:43 +0200

    LinkUtils

--- src/main/java/paulxyh/util/LinkUtils.java
+++ src/main/java/paulxyh/util/LinkUtils.java
@@ -1,35 +1,83 @@
 package paulxyh.util;
 
+import paulxyh.exception.IncorrectInputException;
 import paulxyh.util.logger.Logger;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LinkUtils {
-    public static boolean isLinkValid(String link, List<String> allowedDomains){
+    private static List<String> allowedDomains;
+    private static final Map<String, Boolean> checkedUrls = new HashMap<>();
+    public static void setAllowedDomains(List<String> domains){
+        allowedDomains = domains;
+    }
+    public static boolean isLinkValid(String link){
         try{
-            URL url = new URL(link);
-            String host = url.getHost();
-            return checkReachable(url) && checkDomain(host, allowedDomains);
+            Logger.info("Checking link: " + link);
+            if(isLinkAlreadyChecked(link)){
+                Logger.warn("Link was already checked! Stepping over!");
+                return checkedUrls.get(link);
+            }
+            if(link.contains("mailto") || link.contains("tel:") || link.contains("sms:")){
+                checkedUrls.put(link, true);
+                return true;
+            }
+            if(link.startsWith("file:/")){
+                return checkDomain(link) && checkFileLinkExists(link);
+            }
+            if(link.startsWith("https://") || link.startsWith("http://")) {
+                URL url = new URL(link);
+                String host = url.getHost();
+                boolean isValid = checkReachable(url) && checkDomain(host);
+                checkedUrls.put(link, isValid);
+                return isValid;
+            }
+            return false;
         } catch (IOException e) {
             Logger.error("Url could not be checked: " + e.getMessage());
+            checkedUrls.put(link, false);
             return false;
         }
+    }
+
+    // for testing purposes
+    private static boolean checkFileLinkExists(String link){
+        Path filePath = Paths.get(URI.create(link));
+        return Files.exists(filePath);
+    }
+
+    private static boolean isLinkAlreadyChecked(String link){
+        return checkedUrls.containsKey(link);
     }
 
     private static boolean checkReachable(URL url) throws IOException {
         Logger.info("Checking Reachability of url!");
         HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+        httpURLConnection.setRequestMethod("HEAD");
         return httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK;
     }
 
-    private static boolean checkDomain(String host, List<String> allowedDomains){
+    private static boolean checkDomain(String host){
         Logger.info("Checking if domain is allowed!");
         for(String domain : allowedDomains){
             if(host.contains(domain)) return true;
         }
         return false;
+    }
+
+    public static void checkUrlFormatting(String url) throws IncorrectInputException {
+        if(url.matches("http://.*") || url.matches("https://.*")){
+            return;
+        }
+        throw new IncorrectInputException();
     }
 }
