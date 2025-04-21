7e18e76fa3dcc8375dbbd4564e9f272fbe97a379
paulxyh <77801088+paulxyh@users.noreply.github.com>
2025-04-21 22:44:22 +0200

    HTMLParser

--- src/main/java/paulxyh/util/parser/HTMLParser.java
+++ src/main/java/paulxyh/util/parser/HTMLParser.java
@@ -1,10 +1,11 @@
 package paulxyh.util.parser;
 
 import org.jsoup.nodes.Document;
+import paulxyh.exception.ElementNotRecognizedException;
 import paulxyh.model.PageElement;
 
 import java.util.List;
 
 public interface HTMLParser {
-    List<PageElement> parse(String url, Document content);
+    List<PageElement> parse(String url, Document content) throws ElementNotRecognizedException;
 }
--- src/main/java/paulxyh/util/parser/HTMLParserImpl.java
+++ src/main/java/paulxyh/util/parser/HTMLParserImpl.java
@@ -1,13 +1,60 @@
 package paulxyh.util.parser;
 
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import paulxyh.exception.ElementNotRecognizedException;
+import paulxyh.model.Heading;
+import paulxyh.model.Link;
 import paulxyh.model.PageElement;
+import paulxyh.util.LinkUtils;
 
-import java.util.List;
+import java.util.*;
 
 public class HTMLParserImpl implements HTMLParser{
+    private List<PageElement> htmlElements;
+    private final Map<String, Boolean> checkedUrls = new HashMap<>();
+
     @Override
-    public List<PageElement> parse(String url, Document content) {
-        return List.of();
+    public List<PageElement> parse(String url, Document content) throws ElementNotRecognizedException{
+        htmlElements = new ArrayList<>();
+
+        Elements elements = content.body().select("h1, h2, h3, h4, h5, h6, a[href]");
+
+        for(Element element : elements){
+            parseToCorrectType(element);
+        }
+
+        return htmlElements;
+    }
+
+    private void parseToCorrectType(Element element) throws ElementNotRecognizedException {
+        if(element.tagName().matches("h[1-6]")) {
+            parseHeading(element);
+        }
+        else if (element.tagName().equals("a")) {
+            parseLink(element);
+        }
+        else {
+            throw new ElementNotRecognizedException();
+        }
+    }
+
+    private void parseHeading(Element element) {
+        String tag = element.tagName();
+        int level = Integer.parseInt(tag.substring(1));
+        this.htmlElements.add(new Heading(level, element.text()));
+    }
+
+    private void parseLink(Element element) {
+        String link = element.absUrl("href");
+        boolean isLinkValid;
+        if (checkedUrls.containsKey(link)) {
+            isLinkValid = checkedUrls.get(link);
+        } else {
+            isLinkValid = LinkUtils.isLinkValid(link);
+            checkedUrls.put(link, isLinkValid);
+        }
+        htmlElements.add(new Link(link, isLinkValid));
     }
 }
